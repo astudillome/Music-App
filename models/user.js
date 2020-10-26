724cae8eb0d2a5eb3b3fb1792fa59c53b3538519
lucyefranco <69187546+lucyefranco@users.noreply.github.com>
2020-10-26 13:46:53 -0700

    Revert "Maria- updates models' associations"

--- models/user.js
+++ models/user.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.user.belongsToMany(models.favorite);
     }
   };
   user.init({
