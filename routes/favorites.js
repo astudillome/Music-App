b2c0c2549400d77dd8df62ed660cada61c31f899
lucyefranco <lucyefranco@gmail.com>
2020-10-27 11:13:14 -0700

    adds favorites route, code not checked for functionality

---
+++ routes/favorites.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const passport = require('../config/ppConfig');
+const db = require('../models');
+const router = express.Router();
+const axios = require('axios');
+
+
+//GET /favorites - return a page with favorited albums
+router.get('/', (req, res) =>{
+    //ToDo: get all records from DB and render to view
+    db.favorites.findAll().then(allFavorites => {
+        res.render('favorites', {favorites: allFavorites})
+    })
+})
+
+//POST /favorites - receive album information and add it to the Database
+router.post('/', function(req,res) {
+    //ToDo: Get form data and add it
+    db.pokemon.findOrCreate(req.body).then(newFavorite => {
+        res.redirect('/favorites')
+    })
+})
+
+
+
+module.exports = router;
