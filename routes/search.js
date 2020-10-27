5b8fcdf7f5f3d05c4a5665e0465206ba35804372
astudillome <projectagility@gmail.com>
2020-10-27 11:52:07 -0600

    updates search.ejs, capitalize artist in search results. put partials in conditional header to search when loggedin

--- routes/search.js
+++ routes/search.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const axios = require('axios');
 
 router.post('/', (req, res) =>{
-  console.log("////",req.body.search_input)  
+  let artist = req.body.search_input;  
   axios.get(`https://api.discogs.com/database/search?artist=${req.body.search_input}&key=${process.env.disKey}&secret=${process.env.disSecret}`).then(function(apiResponse) {
         let discogs = apiResponse.data.results;
-        res.render('search-results', {discogs})
+        res.render('search-results', {discogs, artist} )
         })
 })
 
