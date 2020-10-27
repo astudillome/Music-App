fe6930d728dfd576f39a5b5ed4b9d4360938ee78
lucyefranco <lucyefranco@gmail.com>
2020-10-26 18:11:44 -0700

    adds /results route to server. passes functionality test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const app = express();
 
 //Testing API
-app.get('/searchresults', (req, res) => {
+app.get('/results', (req, res) => {
     axios.get(`https://api.discogs.com/database/search?artist=Dolly+Parton&key=${process.env.disKey}&secret=${process.env.disSecret}`).then(function(apiResponse) {
         var discogs = apiResponse.data.results;
         console.log(discogs);
