cf3b425316ba8bdd910dd49c377c82f5a579d453
ctavispost <ctavispost@gmail.com>
2020-10-29 16:49:02 -0700

    Add image for landing page

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -1,4 +1,4 @@
-const url = document.querySelector('.url').innerHTML;
+const url = document.querySelectorAll('.url').innerHTML;
 
 function getId(url){
   let urlString = url.split('=');
@@ -10,4 +10,4 @@ const videoId = getId(url);
 const iframeMarkup = '<iframe width="560" height="315" src="//www.youtube.com/embed/' 
     + videoId + '" frameborder="0" allowfullscreen></iframe>';
 
-document.querySelectorAll('.youtubeCont').innerHTML += iframeMarkup;
+document.querySelector('.youtubeCont').innerHTML += iframeMarkup;
